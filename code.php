3626e123c352a7dcaea9f73f2f369d0272152ca9
Jukka Svahn <web@rahinaa.biz>
2012-02-01 01:25:28 +0200

    Adding v0.2 from SVN.

--- code.php
+++ code.php
@@ -2,7 +2,7 @@
 	##################
 	#
 	#	rah_repeat-plugin for Textpattern
-	#	version 0.1
+	#	version 0.2
 	#	by Jukka Svahn
 	#	http://rahforum.biz
 	#
@@ -11,21 +11,34 @@
 	function rah_repeat($atts,$thing='') {
 		extract(lAtts(array(
 			'delimiter' => ',',
-			'value' => ''
+			'value' => '',
+			'limit' => '',
+			'offset' => ''
 		),$atts));
+		
 		$values = explode($delimiter,$value);
 		
 		if(count($values) == 0)
 			return '';
 		
+		$i = 0;
 		$out = array();
+		
 		global $rah_repeat;
 		foreach($values as $string) {
+			$i++;
+			if(!empty($offset) && $i <= $offset)
+				continue;
 			$rah_repeat = $string;
 			$out[] = parse($thing);
 			$rah_repeat = '';
+			if(!empty($limit) && $i == $limit)
+				break;
 		}
-		unset($rah_repeat);
+		unset(
+			$rah_repeat,
+			$string
+		);
 		return implode('',$out);
 	}
 
